051a9d3cd2e4e2a39de6f1bafa98ca99a23bc880
Михаил Арапов <misha.arapov@gmail.com>
2023-01-31 17:29:41 +0100

    made menu

--- Source.cpp
+++ Source.cpp
@@ -10,8 +10,56 @@ using namespace std;
 
 int main() {
 	travel_data data;
-	start(data);
-	print_data(data);
+	int ch = 0;
+	while (ch != 8) {
+		std::cout << "1. Add data\n";
+		std::cout << "2. Search for person\n";
+		std::cout << "3. Edit details\n";
+		std::cout << "4. Delete person\n";
+		std::cout << "5. Sort people\n";
+		std::cout << "6. List info\n";
+		std::cout << "7. Add person\n";
+		std::cout << "8. Save to file and exit";
+		std::cout << ">>";
+		std::cin >> ch;
+		switch (ch)
+		{
+		case 1:
+		{
+			start(data);
+		}
+		case 2:
+		{
+
+		}
+		case 3:
+		{
+
+		}
+		case 4:
+		{
+
+		}
+		case 5:
+		{
+
+		}
+		case 6:
+		{
+			print_data(data);
+		}
+		case 7:
+		{
+
+		}
+		case 8:
+		{
+			std::cout << "Saved to the file";
+		}
+		default:
+			break;
+		}
+	}
 	/*std::ifstream fin("text.txt");
 	if (fin.is_open()) {
 		travel_data data;
